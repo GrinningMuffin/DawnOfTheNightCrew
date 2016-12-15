03da6f3ac287aeae382843f106f99a41d2c0f7d9
CakieS <goshyjosh@gmail.com>
2016-12-14 21:41:23 -0800

    Initial commit

---
+++ Source/DawnOfTheNightCrew/DawnOfTheNightCrewGameModeBase.h
@@ -0,0 +1,19 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "GameFramework/GameModeBase.h"
+#include "DawnOfTheNightCrewGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class DAWNOFTHENIGHTCREW_API ADawnOfTheNightCrewGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
